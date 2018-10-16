b2e3c5a096561bc9eee5ff525de5ccba00b5d5a3
Eric Yu <eric@aeternalis.co.uk>
2018-10-16 21:23:46 +0100

    initial

---
+++ acc.py
@@ -0,0 +1,34 @@
+class Account:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(filepath, 'r') as file:
+            self.balance = int(file.read())
+
+
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+    def commit(self):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
+
+class Checking(Account):
+
+    def __init__(self, filepath, fee=3):
+        Account.__init__(self, filepath)
+        self.fee = fee
+
+
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+checking = Checking('balance.txt', 5)
+checking.transfer(105)
+print(checking.balance)
+checking.commit()
